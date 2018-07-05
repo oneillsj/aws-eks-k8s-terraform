4a9f7a323235cc7e7017f0466e3a2f700dbe3ca3
Stephen O'Neill <soneill@zendesk.com>
2018-07-05 10:02:13 +0100

    fix

--- cluster/providers.tf
+++ cluster/providers.tf
@@ -2,16 +2,16 @@
 # Provider Configuration
 #
 
-#provider "aws" {
-#  region = "us-east-1"
-#}
-
-
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "us-east-1"
+  region = "us-east-1"
 }
+
+
+#provider "aws" {
+#  access_key = "${var.aws_access_key}"
+#  secret_key = "${var.aws_secret_key}"
+#  region     = "us-east-1"
+#}
 
 # Using these data sources allows the configuration to be
 # generic for any region.
